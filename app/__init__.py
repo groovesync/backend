aec62ecc322e3644b45817f0e457b3f1cb36b6c6
Eli Pedro <eli.pedro.silva@ccc.ufcg.edu.br>
2025-02-10 11:17:00 -0300

    Correções de requerimentos

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from app.routes import auth, user, spotify
 from app.utils.persistence_manager import PersistenceManager
 from app.config import config_dict, Config
 from flask_limiter import Limiter
@@ -11,20 +10,22 @@ limiter = Limiter(get_remote_address, default_limits=["5 per minute"])
 def create_app():
     app = Flask(__name__)
 
-
     env = os.getenv("FLASK_ENV", "development")
     app.config.from_object(config_dict.get(env, Config))
     app.config.from_object(Config)
 
     limiter.init_app(app)
 
+    with app.app_context():  # ✅ Garante que as importações ocorrem dentro do contexto da aplicação
+        from app.routes import auth, user, spotify  # Agora dentro do contexto
+
+        app.register_blueprint(auth.bp)
+        app.register_blueprint(user.bp)
+        app.register_blueprint(spotify.bp)
+
     @app.teardown_appcontext
     def close_db_connection(exception):
         if hasattr(PersistenceManager, "close_connection"):
             PersistenceManager.close_connection()
-
-    app.register_blueprint(auth.bp)
-    app.register_blueprint(user.bp)
-    app.register_blueprint(spotify.bp)
 
     return app
